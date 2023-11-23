686399e67150b397debf093cb99cc1f908026ca6
PavelPer1 <91134079+PavelPer1@users.noreply.github.com>
2023-11-23 16:33:42 +0300

    ...

--- DjangoSites/Mysite/urls.py
+++ DjangoSites/Mysite/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('offers/', include('offers.urls')),
 ]
